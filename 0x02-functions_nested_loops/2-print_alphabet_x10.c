b77b2d641c5964e37059987719a3a994acffe04c
neo2621 <neomakhubo25@gmail.com>
2021-08-25 04:03:14 +0200

    print alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,15 @@
  * Return: nothing.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	int l = 'a';
+       int count = 0;
+	char letter;
 
-	while (l <= 'z')
+	while (count++ <= 9)
 	{
-		_putchar(l);
-		l += 1;
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
-	_putchar(10);
 }
